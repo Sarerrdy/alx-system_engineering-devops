45d1034f79ec6ab7a920a44825fca84b31b81616
sarerrdy <sarerrdy4live@gmail.com>
2024-08-16 05:45:40 +0100

    final commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+""" Function that queries Reddit API and returns the number of subscribers """
+import requests
 
 
-def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
+def number_of_subscribers(subreddit):
+    """ Returns the total number of subscribers """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        data = response.json()
+        return data['data']['subscribers']
+    except (requests.exceptions.RequestException, KeyError):
+        return 0
